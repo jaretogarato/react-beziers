daf751c7ab5ece02bc8c7e106042e81a65f5b411
Jared Gallardo <jaredgg@gmail.com>
2022-11-05 22:07:38 -0600

    complete main smoothing function

--- src/components/Bezier03.js
+++ src/components/Bezier03.js
@@ -35,42 +35,46 @@ class Bezier03 extends Component {
     // C             1000,525
 
     points.map((point, i) => {
-      // var mapPoint = Bezier(i, points);
-      console.log("The current iteration is: " + i);
-      console.log("The current element is: " + point);
-      console.log("points.length: ", points.length);
+      // TODO: use "point" rather than "points[i]"
+
+      // console.log("The current iteration is: " + i);
+      // console.log("The current element is: " + point);
+      // console.log("points.length: ", points.length);
       // console.log("The current mapPoint is: " + mapPoint );
       console.log("\n");
       console.log("i ----- >>> ", i);
       if (i == 0) {
-        cubicPoints += "M " + points[i][0] + "," + points[i][1];
+        cubicPoints += "M " + points[i][0] + "," + points[i][1] + " ";
         console.log("cubicPoints: ", cubicPoints);
       }
       else {
         cp1x = Math.round((points[i][0] + points[i-1][0])/2);
         cp1y = points[i-1][1];
         // cp2x = Math.round((points[i][0] + points[i+1][0])/2);
-        cp2x = (i == points.length-1) ? points[i][0] : Math.round((points[i][0] + points[i+1][0])/2);
+        cp2x = (i == points.length-1) ? points[i][0] : Math.round((points[i][0] + points[i-1][0])/2);
         cp2y = points[i][1];
         ptx = points[i][0];
         pty = points[i][1];
-      }
-      console.log("cp1x: ", cp1x);
-      console.log("cp1y: ", cp1y);
-      console.log("cp2x: ", cp2x);
-      console.log("cp2y: ", cp2y);
-      console.log('ptx: ', ptx);
-      console.log('pty: ', pty);
-      console.log("- - - - - - - - - ");
 
-      // condition ? result if true : result if falsy
+        console.log("cp1x: ", cp1x);
+        console.log("cp1y: ", cp1y);
+        console.log("cp2x: ", cp2x);
+        console.log("cp2y: ", cp2y);
+        console.log('ptx: ', ptx);
+        console.log('pty: ', pty);
+        console.log("- - - - - - - - - ");
+
+        cubicPoints += "C " + cp1x + "," + cp1y + " " + cp2x + "," + cp2y + " " + ptx + "," + pty + " ";
+      }
+
+      console.log ('cubicPoints: ', cubicPoints);
 
       // return mapPoint;
     });
 
     const pathCubic05 = "C 100,50 25,75 25,100 C 25,125 300,150 25,175";
 
-    return pathCubic05;
+    return cubicPoints;
 
   }
 
@@ -80,12 +84,12 @@ class Bezier03 extends Component {
         backgroundColor: 'white',
         border: 'lightblue 10px solid',
         padding: '10px',
-        height: '320px',
+        height: '620px',
         width: '1020px'
       }}>
         <svg
-          viewBox="0 0 1000 300"
-          style={{ maxHeight: 300 }}
+          viewBox="0 0 1000 600"
+          style={{ maxHeight: 600 }}
         >
           <path
             d={
