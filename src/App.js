f707178b1c2643022d87b22fbf6cd07deee7daa8
Jared Gallardo <jaredgg@gmail.com>
2022-11-29 21:16:39 -0700

    move web audio api stuff to separate component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import React, { useContext, useState } from 'react'
+import React, { useState } from 'react'
 import ConsoleHelper from './ConsoleHelper'
 import 'semantic-ui-css/semantic.min.css'
 import { ThemeContext } from './components/themeContext'
 import { Header, Footer, Error404, Home } from './components'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Osc1 from './components/Osc1'
 import './styles/styles.js'
 
-let actx = new AudioContext()
-let out = actx.destination
-let osc1 = actx.createOscillator()
-let gain1 = actx.createGain()
-
-osc1.connect(gain1)
-gain1.connect(out)
-
 function App() {
-	const [osc1Freq, setOsc1Freq] = useState(osc1.frequency.value)
 	const [theme, setTheme] = useState(
 		localStorage.getItem('theme_key') !== null
 			? localStorage.getItem('theme_key')
@@ -29,20 +19,11 @@ function App() {
 		ConsoleHelper('Current theme: ' + theme)
 	}
 
-	const changeOsc1Freq = (e) => {
-		let { value } = e.target
-		setOsc1Freq(value)
-		osc1.frequency.value = value
-	}
-
 	return (
 		<ThemeContext.Provider value={{ theme, toggleTheme }}>
 			<div id='App' className={theme}>
 				<Router>
 					<Header />
-					<button onClick={() => osc1.start()}>Start</button>
-					<button onClick={() => osc1.stop()}>Stop</button>
-					<Osc1 changeFreq={changeOsc1Freq} freq={osc1Freq} />
 					<Routes>
 						<Route path='/' element={<Home />} />
 						<Route path='*' element={<Error404 />} />
--- src/components/WebAudioFileIn.js
+++ src/components/WebAudioFileIn.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Osc1 from './Osc1'
 
-class WebAudioFileIn extends React.Component {
-	render() {
-		return <h1>WebAudioFileIn</h1>
+let actx = new AudioContext()
+let out = actx.destination
+let osc1 = actx.createOscillator()
+let gain1 = actx.createGain()
+
+osc1.connect(gain1)
+gain1.connect(out)
+
+function WebAudioFileIn() {
+	const [osc1Freq, setOsc1Freq] = useState(osc1.frequency.value)
+
+	const changeOsc1Freq = (e) => {
+		let { value } = e.target
+		setOsc1Freq(value)
+		osc1.frequency.value = value
 	}
+
+	return (
+		<>
+			<h1>WebAudioFileIn</h1>
+			<button onClick={() => osc1.start()}>Start</button>
+			<button onClick={() => osc1.stop()}>Stop</button>
+			<Osc1 changeFreq={changeOsc1Freq} freq={osc1Freq} />
+		</>
+	)
 }
 
 export default WebAudioFileIn
